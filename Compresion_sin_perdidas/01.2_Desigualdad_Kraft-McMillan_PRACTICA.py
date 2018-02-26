5e158941fcf3e537970ae0eefda6ee19fa24e5c3
Mmorenogomez <miguelmoregome@gmail.com>
2018-02-26 18:29:51 +0100

    entropia almost done

--- Compresion_sin_perdidas/01.2_Desigualdad_Kraft-McMillan_PRACTICA.py
+++ Compresion_sin_perdidas/01.2_Desigualdad_Kraft-McMillan_PRACTICA.py
@@ -11,8 +11,8 @@ q-ario, decidir si pueden definir un código.
 
 def kraft1(L, q=2):
     sum = 0
-    for li in L:
-        sum += q ** li
+    for l in L:
+        sum += 1/(q ** l)
     return sum <= 1
 
 
@@ -24,6 +24,7 @@ máxima, max(L), que se pueden añadir y seguir siendo un código.
 '''
 
 def kraft2(L, q=2):
+    return kraft3(L, max(L), q)
 
 
 '''
@@ -34,20 +35,26 @@ un código.
 '''
 
 def kraft3(L, Ln, q=2):
-
+    if kraft1(L, q):
+        for l in L:
+            
+    else:
+        return 0
 
 '''
 Dada la lista L de longitudes de las palabras de un  
 código q-ario, hallar un código prefijo con palabras 
 con dichas longitudes
 '''
-def Code(L,q=2):
-
+def Code(L, q=2):
+    return 0
 
 '''
 Ejemplo
 '''
 
-L=[1, 3, 5, 5, 10, 3, 5, 7, 8, 9, 9, 2, 2, 2]
+L = [1, 3, 5, 5, 10, 3, 5, 7, 8, 9, 9, 2, 2, 2]
 print(sorted(L), ' codigo final:', Code(L, 3))
 print(kraft1(L))
+print(kraft2(L))
+#print(kraft3(L, max(L)+1, q))
--- Compresion_sin_perdidas/01.3_entropia_PRACTICA.py
+++ Compresion_sin_perdidas/01.3_entropia_PRACTICA.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-
+NOTA: Aquí (ab)uso las funciones de orden superior de Python!
 """
 import math
 import numpy as np
@@ -11,27 +11,45 @@ import matplotlib.pyplot as plt
 Dada una lista p, decidir si es una distribución de probabilidad (ddp)
 0<=p[i]<=1, sum(p[i])=1.
 '''
-def es_ddp(p,tolerancia=10**(-5)):
-
+def es_ddp(p, tolerancia = 10**(-5)):
+    
+    bool1 = all(map((lambda x: 0 <= x and x <= 1), p))
+    sum_abs = math.fsum(p) - 1
+    
+    bool2 = sum_abs <= tolerancia
+    return bool1 and bool2
 
 
 '''
 Dado un código C y una ddp p, hallar la longitud media del código.
 '''
 
-def LongitudMedia(C,p):
+def LongitudMedia(C, p):
+    acc = 0
+    for i in range(0, len(p)):
+        acc += p[i] * len(C[i])
+    return acc
+        
 
     
 '''
 Dada una ddp p, hallar su entropía.
 '''
 def H1(p):
+    bits = 0.0
+    for Pi in p:
+        if Pi != 0:
+            bits += (-1 * Pi * math.log(Pi, 2))
+    return bits
 
 
 '''
 Dada una lista de frecuencias n, hallar su entropía.
 '''
 def H2(n):
+    SUM_VALS = sum(n)
+    Probs = map((lambda x: x/SUM_VALS), n)
+    return H1(Probs)
 
 
 
@@ -39,24 +57,25 @@ def H2(n):
 '''
 Ejemplos
 '''
-C=['001','101','11','0001','000000001','0001','0000000000']
-p=[0.5,0.1,0.1,0.1,0.1,0.1,0]
-n=[5,2,1,1,1]
+C = ['001', '101', '11', '0001', '000000001', '0001', '0000000000']
+p = [0.5, 0.1, 0.1, 0.1, 0.1, 0.1, 0]
+n = [5, 2, 1, 1, 1]
 
+print( es_ddp(p, 10**(-5)) )
 print(H1(p))
 print(H2(n))
 print(LongitudMedia(C,p))
 
+''' --------------------------------------------------------------- '''
+
+'''
+Dibujar H(p, 1-p)
+'''
+
 
 
 '''
-Dibujar H(p,1-p)
-'''
-
-
-
-'''
-Hallar aproximadamente el máximo de  H(p,q,1-p-q)
+Hallar aproximadamente el máximo de  H(p, q, 1-p-q)
 '''
 
 
