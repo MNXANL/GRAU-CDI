1c2affa144eecaed27fba04d8026f5f0a044c72a
Mmorenogomez <miguelmoregome@gmail.com>
2018-02-26 19:50:58 +0100

    missing kraft

--- Compresion_sin_perdidas/01.2_Desigualdad_Kraft-McMillan_PRACTICA.py
+++ Compresion_sin_perdidas/01.2_Desigualdad_Kraft-McMillan_PRACTICA.py
@@ -9,10 +9,14 @@ q-ario, decidir si pueden definir un código.
 
 '''
 
-def kraft1(L, q=2):
+def kraftSum(L, q):
     sum = 0
     for l in L:
         sum += 1/(q ** l)
+    return sum
+
+def kraft1(L, q=2):
+    sum = kraftSum(L, q)
     return sum <= 1
 
 
@@ -36,10 +40,16 @@ un código.
 
 def kraft3(L, Ln, q=2):
     if kraft1(L, q):
-        for l in L:
-            
-    else:
-        return 0
+        alpha = kraftSum(L, q)
+        K = 0
+        i = 0
+        while K+alpha <= 1 and i < len(L):
+            if len(L[i]) == len(Ln):
+                K += (1/L[i])
+                i += 1
+        return K
+                
+    else: return 0
 
 '''
 Dada la lista L de longitudes de las palabras de un  
@@ -57,4 +67,4 @@ L = [1, 3, 5, 5, 10, 3, 5, 7, 8, 9, 9, 2, 2, 2]
 print(sorted(L), ' codigo final:', Code(L, 3))
 print(kraft1(L))
 print(kraft2(L))
-#print(kraft3(L, max(L)+1, q))
+print(kraft3(L, max(L)+1, 2))
--- Compresion_sin_perdidas/01.3_entropia_PRACTICA.py
+++ Compresion_sin_perdidas/01.3_entropia_PRACTICA.py
@@ -5,6 +5,7 @@ NOTA: Aquí (ab)uso las funciones de orden superior de Python!
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 '''
@@ -13,9 +14,11 @@ Dada una lista p, decidir si es una distribución de probabilidad (ddp)
 '''
 def es_ddp(p, tolerancia = 10**(-5)):
     
-    bool1 = all(map((lambda x: 0 <= x and x <= 1), p))
-    sum_abs = math.fsum(p) - 1
+    ''' Check si 0 <= p[i] <= 1 '''
+    bool1 = all( map((lambda x: 0 <= x and x <= 1), p) )
     
+    ''' Check si sum(p[i]) == 1 usando tolerancias '''
+    sum_abs = math.fsum(p) - 1
     bool2 = sum_abs <= tolerancia
     return bool1 and bool2
 
@@ -66,17 +69,44 @@ print(H1(p))
 print(H2(n))
 print(LongitudMedia(C,p))
 
-''' --------------------------------------------------------------- '''
+''' 
+-------------------------------------------------------------------------------
+ 
 
-'''
 Dibujar H(p, 1-p)
 '''
-
-
+def DrawH(step = 0.01):
+    X = list()
+    Y = list()
+    for p in np.arange(0.0, 1.0, step):
+        Z = [p, 1-p]
+        X.append(p)
+        Y.append(H1(Z))
+    plt.plot(X, Y)
+        
+DrawH()
 
 '''
 Hallar aproximadamente el máximo de  H(p, q, 1-p-q)
 '''
-
+def MaxH(step = 0.01):
+    
+    Max = 0.0
+    #X = list()
+    #Y = list()
+    #Z = list()
+    for p in np.arange(0.0, 1.0, step):
+        for q in np.arange(0.0, 1.0, step):
+            if (1-p-q >= 0):
+                Z = [p, q, 1-p-q]
+                entropy = H1(Z)
+                #X.append(p)
+                #Y.append(q)
+                #Z.append(entropy)
+                if Max < entropy:  Max = entropy
+    
+    return Max
+        
+print("max{ H(p, q, 1-p-q) } = ", MaxH())
 
 
